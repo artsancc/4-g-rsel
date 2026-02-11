59b30f6ece642d9b9488e01099ea5f47cf39e8c0
artsancc <arzunurtunal@gmail.com>
2026-02-11 16:00:29 +0300

    Add files via upload

--- 3/blackswan.py
+++ 3/blackswan.py
@@ -12,7 +12,7 @@ KALIN = 5
 ORTA = 3
 INCE = 1
 
-dosya_adi = "alice.jpg"
+dosya_adi = "blackswan.jpg"
 img_ham = cv2.imread(dosya_adi)
 
 print(f"Görüntü Verisi Yüklendi.")
@@ -20,7 +20,7 @@ print(f"Matris Boyutu: {img_ham.shape}")
 #orjinal veriyi korumak için kopyayla çalışıyoruz.
 img = img_ham.copy()
 font = cv2.FONT_HERSHEY_SIMPLEX
-cv2.putText(img, "Alice Cizim", (50, 50), font, 1, BEYAZ, 2)
+cv2.putText(img, "Black Swan", (50, 50), font, 1, BEYAZ, 2)
 
 print("\n--- KONTROLLER ---")
 print("c: Daire (Circle) - Mavi")
@@ -42,15 +42,15 @@ while True:
         break
 
     elif tus == ord('c'):
-        cv2.circle(img, (350, 600), 100, MAVI, ORTA)
+        cv2.circle(img, (465, 130), 70, MAVI, ORTA)
         print("Veri Güncellendi: Mavi Daire Eklendi.")
 
     elif tus == ord('r'):
-        cv2.rectangle(img, (100, 450), (250, 200), YESIL, ORTA)
+        cv2.rectangle(img, (800, 500), (100, 300), YESIL, ORTA)
         print("Veri Güncellendi: Yeşil Dikdörtgen Eklendi.")
 
     elif tus == ord('l'):
-        cv2.line(img, (130, 700), (630, 700), SARI, KALIN)
+        cv2.line(img, (850, 200), (80, 180), SARI, KALIN)
         print("Veri Güncellendi: Kırmızı Çizgi Eklendi.")
 
     # Başlangıca dönme
